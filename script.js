80899367da7feaefcc5639e4820af7aa8516d48d
RoyTrex <61310024+RoyTrex@users.noreply.github.com>
2025-01-23 18:47:51 +0300

    Complete game function with loop and rounds logic. Remove unnecessary code

--- script.js
+++ script.js
@@ -10,19 +10,17 @@ function getComputerChoice() {
   return compChoice;
 }
 
+
 // User Choice Logic
 
 function getUserChoice() {
   let userChoice = prompt("Pick 'Rock', 'Paper' or 'Scissors'!");
 
   if (userChoice.toLowerCase() === "rock") {
-    // userChoice = userChoice.toLowerCase();
     console.log("You've chosen 'Rock'");
   } else if (userChoice.toLowerCase() === "paper") {
-    // userChoice = userChoice.toLowerCase();
     console.log("You've chosen 'Paper'");
   } else if (userChoice.toLowerCase() === "scissors") {
-    // userChoice = userChoice.toLowerCase();
     console.log("You've chosen 'Scissors'");
   } else {
     console.log("Please pick a valid option! 'Rock', 'Paper' or 'Scissors'.")
@@ -32,56 +30,54 @@ function getUserChoice() {
   return userChoice;
 }
 
-// Score Tracking
 
-let humanScore = 0;
-let computerScore = 0;
+// Game Logic
 
-// Round Logic
+function playGame() {
+  let userScore = 0;
+  let computerScore = 0;
 
-function playRound(userChoice, computerChoice) {
-  userChoice = userChoice.toLowerCase();
+  // Round Logic
 
-  if (userChoice === computerChoice) {
-    console.log("Tie!");
-  } else if (
-    userChoice === 'rock' && computerChoice === 'scissors'
-    || userChoice === 'paper' && computerChoice === 'rock'
-    || userChoice === 'scissors' && computerChoice === 'paper'
-    ) {
+  function playRound(userChoice, computerChoice) {
+    userChoice = userChoice.toLowerCase();
+
+    if (userChoice === computerChoice) {
+      console.log("Tie!");
+    } else if (
+      userChoice === 'rock' && computerChoice === 'scissors'
+      || userChoice === 'paper' && computerChoice === 'rock'
+      || userChoice === 'scissors' && computerChoice === 'paper'
+      ) {
+        choiceOne = userChoice.at(0).toUpperCase() + userChoice.slice(1);
+        choiceTwo = computerChoice.at(0).toUpperCase() + computerChoice.slice(1);
+
+        userScore += 1;
+        console.log(`You win! ${choiceOne} beats ${choiceTwo}`);
+    } else {
       choiceOne = userChoice.at(0).toUpperCase() + userChoice.slice(1);
       choiceTwo = computerChoice.at(0).toUpperCase() + computerChoice.slice(1);
+    
+      computerScore += 1;
+      console.log(`You lost! ${choiceTwo} beats ${choiceOne}`);
+    }
+  }
 
-      console.log(`You win! ${choiceOne} beats ${choiceTwo}`);
-  } else if (
-    userChoice === "rock" && computerChoice === "paper"
-    || userChoice === "paper" && computerChoice === "scissors"
-    || userChoice === "scissors" && computerChoice === "rock"
-  ) {
-    choiceOne = userChoice.at(0).toUpperCase() + userChoice.slice(1);
-    choiceTwo = computerChoice.at(0).toUpperCase() + computerChoice.slice(1);
+  // Loop Logic
+
+  for (let i = 1; i < 6; i++) {
+    console.log(`Round ${i}:`);
+    playRound(getUserChoice(), getComputerChoice());
+    console.log("-----");
+  }
+
+  // Announce Winner
   
-    console.log(`You lost! ${choiceTwo} beats ${choiceOne}`);
+  if (userScore > computerScore) {
+    console.log(`You've won this game with a score of ${userScore} to ${computerScore}. Congratulations!`);
+  } else {
+    console.log(`You've lost this game with a score of ${userScore} to ${computerScore}. Better luck next time.`);
   }
 }
 
-const userSelection = getUserChoice();
-const computerSelection = getComputerChoice();
-
-playRound(userSelection, computerSelection);
-
-
-
-// Possibilities:
-// when User = Rock
-//  Rock x Rock = Tie
-//  Rock x Paper = Lose
-//  Rock x Scissors = Win
-// when User = Paper
-//  Paper x Rock = Win
-//  Paper x Paper = Tie
-//  Paper x Scissors = Lose
-// when User = Scissors
-//  Scissors x Rock = Lose
-//  Scissors x Paper = Win
-//  Scissors x Scissors = Tie
+playGame();
